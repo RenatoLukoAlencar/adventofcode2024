7a675495719349ce8f7c6cf087c26eaf5b7b4fda
Renato Alencar <renato.luko@yahoo.com>
2024-12-12 00:09:55 -0300

    6.1 completed

--- day6.js
+++ day6.js
@@ -9,63 +9,97 @@ try {
   //   for (let line of table) {
   //     console.log(line);
   //   }
-  let count = 0;
-  let blocked = false;
-  let [x, y] = [0, 0];
-  let ns = -1;
-  let we = 0;
-  let rotate_index = 0;
 
-  function rotate() {
-    const compass = [
-      [-1, 0],
-      [0, 1],
-      [1, 0],
-      [0, -1],
-    ];
-    [ns, we] = compass[rotate_index];
-    rotate_index += rotate_index == 3 ? -3 : 1;
-  }
+  let loop_count = 0;
+  let iteration_count = 0;
+  let max_iteration = 50000;
 
-  for (let [index, str] of table.entries()) {
-    if (str.indexOf("^") >= 0) {
-      x = str.indexOf("^");
-      y = index;
-      break;
-    }
-  }
+  for (let omega of table.entries()) {
+    for (let delta of omega[1].entries()) {
+      iteration_count++;
+      table[omega[0]][delta[0]] = "#";
+      console.log(
+        "iniciando loop ",
+        [omega[0], delta[0]],
+        " - ",
+        iteration_count,
+        "/",
+        130 * 130,
+        " - ",
+        "loop_counting",
+        loop_count
+      );
 
-  while (!blocked) {
-    //console.log("x", x, "y", y);
-    let [ny, nx] = [y + ns, x + we];
-    // console.log("nx", nx, "ny", ny);
-    if (ny < 0 || ny >= 130 || nx < 0 || nx >= 129) {
-      blocked = true;
-      break;
-    }
+      let count = 0;
+      let blocked = false;
+      let [x, y] = [0, 0];
+      let ns = -1;
+      let we = 0;
+      let rotate_index = 0;
+      let rotaded = false;
 
-    if (table[ny][nx] == "#") {
-      rotate();
-      [ny, nx] = [y + ns, x + we];
-    } else {
-      table[y][x] = "O";
-      x = nx;
-      y = ny;
-      table[y][x] = "^";
-      count++;
-    }
-  }
-
-  let final_sum = 0;
-  for (let line of table) {
-    console.log(JSON.stringify(line.join("")));
-    for (let char of line) {
-      if (char == "O") {
-        final_sum++;
+      function rotate() {
+        const compass = [
+          [-1, 0],
+          [0, 1],
+          [1, 0],
+          [0, -1],
+        ];
+        [ns, we] = compass[rotate_index];
+        rotate_index += rotate_index == 3 ? -3 : 1;
+        rotaded = true;
       }
+
+      for (let [index, str] of table.entries()) {
+        if (str.indexOf("^") >= 0) {
+          x = str.indexOf("^");
+          y = index;
+          break;
+        }
+      }
+
+      while (!blocked && count <= max_iteration) {
+        if (count % 2000 == 0) {
+          console.log("count: " + count);
+        }
+        let [ny, nx] = [y + ns, x + we];
+        // console.log("nx", nx, "ny", ny);
+        if (ny < 0 || ny >= 130 || nx < 0 || nx >= 129) {
+          blocked = true;
+          break;
+        }
+
+        if (table[ny][nx] == "#") {
+          rotate();
+          [ny, nx] = [y + ns, x + we];
+        } else {
+          table[y][x] = rotaded ? "+" : ns != 0 ? "|" : "-";
+          if (table[y][x] == "+") {
+            rotaded = false;
+          }
+          x = nx;
+          y = ny;
+          table[y][x] = "^";
+          count++;
+        }
+      }
+
+      if (count == max_iteration) {
+        loop_count++;
+      }
+      /*
+      let final_sum = 0;
+      for (let line of table) {
+        //console.log(JSON.stringify(line.join("")));
+        for (let char of line) {
+          if (["|", "-", "^", "+"].indexOf(char) >= 0) {
+            final_sum++;
+          }
+        }
+      }
+        */
     }
   }
-  console.log("sum: ", final_sum, "x", x, "y", y);
 } catch (error) {
   console.log(error);
 }
