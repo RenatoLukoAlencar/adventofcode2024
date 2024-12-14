b661c9fd85d511b4490b834cc4d2727916282d10
Renato Alencar <renato.luko@yahoo.com>
2024-12-14 19:45:35 -0300

    7.2 completed

--- day7.js
+++ day7.js
@@ -11,111 +11,73 @@ try {
     if (res.status) {
       possible.push(res.answer);
     } else {
-      for (let x = 1; x < res.numbers.length; x++) {
-        let temp = res.numbers.slice(0);
-        temp.splice(x, 0, "||");
-        let concat = temp.join(",").replace(",||,", "||").split(",").join(" ");
-
-        let newLine = String(res.answer) + ":" + concat;
-        //console.log("newLine? ", newLine);
-        let newTry = check(newLine);
-        //console.log("newTry ", newLine, "res? ", JSON.stringify(newTry));
-        if (newTry.status) {
-          //console.log("new possible? ", concat);
-          possible.push(res.answer);
-        }
-      }
+      notPossible.push(res.answer);
     }
   }
 
-  console.log("possibles? ", possible.join(","));
+  console.log("possible: ", possible);
   console.log(
-    "sum? ",
+    "sum: ",
     possible.reduce((x, y) => x + y)
   );
 } catch (error) {
   console.log(error);
 }
 
-function d2b(dec) {
-  return (dec >>> 0).toString(2);
+function d2b(dec, base) {
+  let op = {
+    0: "+",
+    1: "*",
+    2: "||",
+  };
+
+  let res = (dec >>> 0)
+    .toString(3)
+    .padStart(base, "0")
+    .split("")
+    .map((x) => op[x]);
+
+  return res;
 }
 
 function check(line) {
   let temp = line.split(":");
-  let op = {
-    0: "+",
-    1: "*",
-  };
   let answer = parseInt(temp[0]);
   let numbers = temp[1].trim().split(" ");
-  let qtd = numbers.length - 1;
-  let success = false;
-  let baseNumber = 0;
-
-  if (numbers.length == 1 && numbers[0].replace("||", "") == answer) {
-    return {
-      status: true,
-      answer: answer,
-      numbers: numbers,
-    };
-  }
-
-  while (String(d2b(baseNumber)).length <= qtd) {
-    let bin = String(d2b(baseNumber))
-      .padStart(numbers.length - 1, "0")
-      .split("")
-      .map((x) => op[x]);
-
-    let total = 0;
-
-    for (let n = 0; n < numbers.length; n++) {
-      if (n == 0) {
-        total = numbers[0].includes("||")
-          ? parseInt(numbers[0].replace("||", ""))
-          : parseInt(numbers[0]);
-
-        //console.log("total? ", total);
-      } else {
-        let current = bin.shift();
-        //console.log("current operation ? ", total, current, numbers[n]);
-        let stepNumber = numbers[n];
-
-        //console.log(stepNumber, ":::::", stepNumber.includes("||"));
-
-        if (current == "+") {
-          total = stepNumber.includes("||")
-            ? parseInt(
-                String(total + parseInt(stepNumber.split("||")[0])) +
-                  stepNumber.split("||")[1]
-              )
-            : total + parseInt(numbers[n]);
-        } else {
-          total = stepNumber.includes("||")
-            ? parseInt(
-                String(total * parseInt(stepNumber.split("||")[0])) +
-                  stepNumber.split("||")[1]
-              )
-            : total * parseInt(numbers[n]);
-        }
-
-        //console.log("total? ", total);
-      }
-    }
-
-    if (total == answer) {
-      success = true;
-      break;
-    }
-    baseNumber++;
-  }
-
-  return {
-    status: success,
+  let res = {
+    status: false,
     answer: answer,
     numbers: numbers,
   };
-}
+  //console.log("numbers: ", temp[1]);
 
-// to low
-// 903282850808
+  let options = [];
+  for (let i = 0; i < 3 ** (numbers.length - 1); i++) {
+    options.push(d2b(i, numbers.length - 1));
+  }
+
+  for (let op of options) {
+    //console.log(op);
+    let output = parseInt(numbers[0]);
+
+    for (let o = 0; o < op.length; o++) {
+      if (op[o] == "+") {
+        output += parseInt(numbers[o + 1]);
+      } else if (op[o] == "*") {
+        output *= parseInt(numbers[o + 1]);
+      } else if (op[o] == "||") {
+        output = parseInt(String(output) + "" + String(numbers[o + 1]));
+      }
+    }
+
+    if (output == answer) {
+      return {
+        status: true,
+        answer: answer,
+        numbers: numbers,
+      };
+    }
+  }
+
+  return res;
+}
--- test.js
+++
@@ -1,8 +0,0 @@
-function d2b(dec) {
-  return (dec >>> 0).toString(2);
-}
-
-let count = 15;
-console.log(count.toString(16));
-
-console.log(parseInt(count.toString(16), 3));
